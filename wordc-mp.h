74f43f14d614138cebb3624eb1b32bb8caa1e580
Ryan Mysliwiec <retroshade@users.noreply.github.com>
2016-03-14 16:15:09 -0400

    Added code from wordc.h

--- wordc-mp.h
+++ wordc-mp.h
@@ -1,1 +1,34 @@
+//
+//  wordc.h
+//  
+//
 
+#ifndef wordc_h
+#define wordc_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <ctype.h>
+#include <time.h>
+#include <string.h>
+
+struct list {
+    char word[30];
+    int count;
+    struct list *next;
+};
+
+typedef int bool;
+#define true 1
+#define false 0
+
+// function prototypes
+struct list* add_first_list_element (char val[30], int j);
+struct list* add_additional_list_element (char val[30], int j);
+bool search_list (char val[30]);
+struct list* find_existing_node (char val[30]);
+struct list* find_prior_node (char val[30]);
+void print_list (FILE *file2);
+
+#endif /* wordc_h */
